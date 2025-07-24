e85b02edbc4182cd4dcd8579aeed347e97c1b6da
Orlando Vasquez <orlando.vasquez@yahoo.com>
2025-07-24 14:37:57 -0500

    Commit en la nueva ram

--- src/holamundo/MiPrimerGit.java
+++ src/holamundo/MiPrimerGit.java
@@ -1,6 +1,7 @@
-
+package src.holamundo;
 public class MiPrimerGit {
     public static void main(String[] args) {
         System.out.println("Este es mi primer proyecto controlado con Git.");
+        System.out.println("Ajuste en rama Ajuste1.");
     }
 }
