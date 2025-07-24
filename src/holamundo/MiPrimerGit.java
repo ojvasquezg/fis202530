081323452fbb7fc8e3181c0f6643d02d3079e4d0
ojvasquezg <123209655+ojvasquezg@users.noreply.github.com>
2025-07-24 14:51:43 -0500

    Ajuste desde github

--- src/holamundo/MiPrimerGit.java
+++ src/holamundo/MiPrimerGit.java
@@ -3,5 +3,7 @@ public class MiPrimerGit {
     public static void main(String[] args) {
         System.out.println("Este es mi primer proyecto controlado con Git.");
         System.out.println("Ajuste en rama Ajuste1.");
+        System.out.println("Ajuste desde github.");
+
     }
 }
