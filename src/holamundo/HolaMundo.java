f77796aa2bdc1195c004a1a9b896056372510724
ojvasquezg <123209655+ojvasquezg@users.noreply.github.com>
2025-07-24 14:33:33 -0500

    Update HolaMundo.java

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -4,6 +4,8 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, mundo!");
         System.out.println("¡Despues del commit!");
+        System.out.println("¡Modificado por otra persona!");
+        
 
     }
 }
