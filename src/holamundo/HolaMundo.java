d419e844f97376304ba178ecb614ffe64ca510ad
Orlando Vasquez <orlando.vasquez@yahoo.com>
2025-07-25 14:53:11 -0500

    Pal commit de luchito

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -7,6 +7,8 @@ public class HolaMundo {
         System.out.println("¡Modificado por otra persona!");
         System.out.println("¡Ajuste en la rama ajuste1 desde Gihub!");
 	System.out.println ("Con source tree");
+	System.out.println ("Con Luchito 3 sera que si?"); 
+
 
     }
 }
